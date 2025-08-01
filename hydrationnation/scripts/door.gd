3e3d767d3108c309fc1e5c5508a7ab40f0e38f6f
candace.govender <candace@psybergate.co.za>
2025-08-01 21:10:24 +0200

    added obstacle and empty door class

---
+++ hydrationnation/scripts/door.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+class_name Door
+
+@export var next_room:String
+@export var spawn_position:Vector2
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
