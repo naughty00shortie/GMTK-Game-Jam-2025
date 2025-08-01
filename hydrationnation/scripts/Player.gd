981243f3efad182edd70080ba8db052bb8e71be2
candace.govender <candace@psybergate.co.za>
2025-08-01 22:05:52 +0200

    added door scene

---
+++ hydrationnation/scripts/Player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+class_name Player
+const SPEED = 100.0
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = direction * SPEED
+	move_and_slide()
+	
+	if velocity.length() > 0:
+		play_walk_animation()
+	else:
+		play_idle_animation()
+
+func play_idle_animation():
+	$AnimatedSprite2D.play("idle")
+
+func play_walk_animation():
+	$AnimatedSprite2D.play("walk")
+
--- hydrationnation/scripts/door.gd
+++ hydrationnation/scripts/door.gd
@@ -2,15 +2,17 @@ extends Area2D
 
 class_name Door
 
-@export var next_room:String
-@export var spawn_position:Vector2
-
+@export var destination_level:String
+@export var destination_door:String
+# @export var spawn_position:Vector2
+@export var spawn_position = "up"
+@onready var spawn = $spawn
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# func _on_body_entered(body:Node2D) -> void:
+	# if body is Player:
+	
+	# pass # Replace with function body.
