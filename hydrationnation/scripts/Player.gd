216d619ba0fa85ad2710a153ffaa161c29cc8e81
albertus van der walt <albertus@psybergate.co.za>
2025-08-02 10:33:26 +0200

    added animations and doors

--- hydrationnation/scripts/Player.gd
+++ hydrationnation/scripts/Player.gd
@@ -1,12 +1,35 @@
 extends CharacterBody2D
 
 class_name Player
-const SPEED = 100.0
+
+@export var speed := 50
+
+@onready var animator = $AnimatedSprite2D
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = direction * SPEED
+	
+	var direction = Vector2.ZERO
+	
+
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		
+		if abs(direction.x) > abs(direction.y):
+			animator.play("walk_horizontal")
+			animator.flip_h = direction.x < 0
+		else:
+			if direction.y > 0:
+				animator.play("walk_down")
+			else:
+				animator.play("walk_up")
+	else:
+		animator.play("idle") 
+	
+	velocity = direction * speed
 	move_and_slide()
 
 	if velocity.length() > 0:
@@ -16,11 +39,13 @@ func _physics_process(delta: float) -> void:
 
 func play_idle_animation():
 	$AnimatedSprite2D.play("idle")
+	
 func takeDamage(damage: int):
 	pass
 
 func play_walk_animation():
 	$AnimatedSprite2D.play("walk")
+	
 func attack():
 	pass
 
--- hydrationnation/scripts/door.gd
+++ hydrationnation/scripts/door.gd
@@ -3,16 +3,12 @@ extends Area2D
 class_name Door
 
 @export var destination_level:String
-@export var destination_door:String
 # @export var spawn_position:Vector2
-@export var spawn_position = "up"
-@onready var spawn = $spawn
+# @export var spawn_position = "up"
+# @onready var spawn = $spawn
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-# func _on_body_entered(body:Node2D) -> void:
-	# if body is Player:
+func _on_body_entered(body:Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file(destination_level)
 	
-	# pass # Replace with function body.
