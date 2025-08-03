2723375794c260f4b729544da60a734467ba9127
Nikhil <nikhilpillay1@gmail.com>
2025-08-03 12:41:27 +0200

    added fishing

--- hydrationnation/scripts/Player.gd
+++ hydrationnation/scripts/Player.gd
@@ -8,6 +8,9 @@ var last_direction := Vector2.RIGHT
 @onready var animator = $AnimatedSprite2D
 @onready var fishing_rod = $FishingRod
 
+enum FishingState { IDLE, CASTING, WAITING, REELING }
+var fishing_state = FishingState.IDLE
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 func _physics_process(delta: float) -> void:
@@ -38,7 +41,7 @@ func _physics_process(delta: float) -> void:
 				animator.flip_h = false;
 				fishing_rod.flip_h = false;
 				fishing_rod.z_index = 2;
-				fishing_rod.position = Vector2(32, -8)
+				fishing_rod.position = Vector2(-1, 0)
 				
 		#moving vertically
 		else: 
@@ -48,7 +51,7 @@ func _physics_process(delta: float) -> void:
 				animator.play("walk_down")
 				fishing_rod.flip_h = false;
 				fishing_rod.z_index = 2;
-				fishing_rod.position = Vector2(32, -8)
+				fishing_rod.position = Vector2(-1, 0)
 				
 			#moving up
 			else:
@@ -77,10 +80,37 @@ func _physics_process(delta: float) -> void:
 				animator.play("idle_down")
 				fishing_rod.z_index = 2;
 				fishing_rod.flip_h = false;
-				fishing_rod.position = Vector2(32, -8)
+				fishing_rod.position = Vector2(-1, 0)
 	
 	velocity = direction * speed
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("fish"):
+		speed = 0;
+		match fishing_state:
+			FishingState.IDLE:
+				fishing_rod.play("cast")
+			FishingState.WAITING:
+				if $Exclamation.visible:
+					fishing_rod.play("reel")
+					fishing_state = FishingState.REELING
+					start_catch_timer()
+				else:
+					fishing_rod.play("catch")
+					speed = 50;
+					fishing_rod.frame = 0
+					fishing_state = FishingState.IDLE
+			FishingState.REELING:
+				
+				if $Exclamation.visible:
+					print("You got a fish!")
+				
+				fishing_rod.play("catch")
+				speed = 50;
+				fishing_rod.frame = 0
+				fishing_state = FishingState.IDLE
+				
+			
 
 func takeDamage(damage: int):
 	pass
@@ -92,7 +122,41 @@ func die():
 	pass
 	
 func _ready():
+	fishing_rod.frame = 0
 	update_rod_visibility()
+	fishing_rod.animation_finished.connect(_on_casting_animation_finished)
+	
+func _on_casting_animation_finished():
+	if fishing_rod.animation == "cast":
+		fishing_state = FishingState.WAITING
+		fishing_rod.play("wait")
+		start_wait_timer()
+		
+func start_wait_timer():
+	await get_tree().create_timer(3.0).timeout
+	if fishing_state == FishingState.WAITING:
+		$Exclamation.position = fishing_rod.position + Vector2(15, -60)	
+		$Exclamation.visible = true;
+		await get_tree().create_timer(1).timeout
+		$Exclamation.visible = false;
+		
+		if fishing_state != FishingState.REELING:
+			fishing_rod.play("catch")
+			speed = 50;
+			fishing_rod.frame = 0
+			fishing_state = FishingState.IDLE
+			
+func start_catch_timer():
+	await get_tree().create_timer(3.0).timeout
+	$Exclamation.visible = true;
+	await get_tree().create_timer(1).timeout
+	$Exclamation.visible = false;
+	
+	#if fishing_state != FishingState.REELING:
+			#fishing_rod.play("catch")
+			#speed = 50;
+			#fishing_rod.frame = 0
+			#fishing_state = FishingState.IDLE
 	
 func update_rod_visibility():
 	var current_scene = get_tree().current_scene
